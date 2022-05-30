cd45dc911533b5e1b1e1da3015a6c85c8bc6b28d
praconfi <praconfi@gmail.com>
2022-05-30 15:36:30 +0900

    홈화면 작업

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,21 @@ class _MyAppState extends State<MyApp> {
         ListenableProvider(create: (_) => MapProvider()),
       ],
       child: MaterialApp(
+
         title: 'dash_moment',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+            bodyText2: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w700),
+
+            // bodyText2가 기본 텍스트 스타일로 적용됨
+            // bodyText1: TextStyle(color: Colors.black),
+            // headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            // appbar타이틀
+            // headline6: TextStyle(color: Colors.black, fontSize: 16),
+          ),
         ),
+        debugShowCheckedModeBanner: false,
         home: MapScreen(),
       ),
     );
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -26,7 +26,7 @@ class _MapScreenState extends State<MapScreen> {
   int markerSizeMedium = Platform.isIOS ? 65 : 45;
   GlobalKey? _keyGoogleMap = GlobalKey();
   bool _isCameraReCenter = false;
-  static const CameraPosition _kGooglePlex =  CameraPosition(
+  static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(0.347596, 32.582520),
     zoom: 14.4746,
   );
@@ -35,18 +35,17 @@ class _MapScreenState extends State<MapScreen> {
 
   CircleId? selectedCircle;
 
-
-
-  void getCurrentPosition() async{
+  void getCurrentPosition() async {
     LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     currentPosition = position;
     LatLng latlngPosition = LatLng(position.latitude, position.longitude);
-    CameraPosition cameraPosition = new CameraPosition(target: latlngPosition, zoom: 14);
-    _mapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latlngPosition, zoom: 14);
+    _mapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
   }
-
-
 
   @override
   void initState() {
@@ -72,37 +71,136 @@ class _MapScreenState extends State<MapScreen> {
   //       });
   // }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return Material(
 
-      child: SlidingUpPanel(
-        backdropEnabled: true,
-        body: Scaffold(
-            body: GoogleMap(
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              zoomGesturesEnabled: true,
-              zoomControlsEnabled: true,
-              mapToolbarEnabled: false,
-              key: _keyGoogleMap,
-              markers: _showMarkers,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _mapController = controller;
-                _mapController.setMapStyle(_mapStyle);
-                getCurrentPosition();
-              },
-              onCameraIdle: () {
-                setState(() {
-                  _isCameraReCenter = false;
-                });
-              },
-            )
-        ),
-        panel: Center(child: Text('panel'),),
+    BorderRadiusGeometry slidingPanelRadius = const BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
+
+    return Material(
+      child: Stack(
+        children: [
+          SlidingUpPanel(
+
+            minHeight: 50,
+            maxHeight: 280,
+            color: Color(0xFF111111).withOpacity(0.8),
+            borderRadius: slidingPanelRadius,
+            backdropEnabled: true,
+            body: SafeArea(
+              child: Scaffold(
+                  body: Stack(children: [
+                    GoogleMap(
+                      myLocationEnabled: true,
+                      myLocationButtonEnabled: true,
+                      zoomGesturesEnabled: true,
+                      zoomControlsEnabled: true,
+                      mapToolbarEnabled: false,
+                      key: _keyGoogleMap,
+                      markers: _showMarkers,
+                      initialCameraPosition: _kGooglePlex,
+                      onMapCreated: (GoogleMapController controller) {
+                        _mapController = controller;
+                        _mapController.setMapStyle(_mapStyle);
+                        getCurrentPosition();
+                      },
+                      onCameraIdle: () {
+                        setState(() {
+                          _isCameraReCenter = false;
+                        });
+                      },
+                    ),
+                    Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.white,
+                                Colors.white,
+                                Colors.white70,
+                                Colors.white30
+                              ]
+                          )
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Opacity(opacity: 0, child: Padding(
+                            padding: EdgeInsets.only(right: 20.0),
+                            child: CircleAvatar(),
+                          )),
+                          Text('서울특별시 강남구 선릉로551',),
+                          Padding(
+                            padding: EdgeInsets.only(right: 20.0),
+                            child: SizedBox(height: 33, width: 33, child: CircleAvatar(foregroundColor: Colors.white, backgroundColor: Colors.black, child: Icon(Icons.person),)),
+                          )
+                        ],
+                      ),
+                    )
+                  ])),
+            ),
+            panel: Column(
+              children: [
+                SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 34,
+                      height: 4,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // the fab
+          Positioned(
+            right: 20.0,
+            bottom: 60,
+            child: SizedBox(
+              height: 55,
+              width: 55,
+              child: FloatingActionButton(
+                backgroundColor: Color(0xFF111111),
+                foregroundColor: Colors.white,
+
+                child: const Icon(
+                  Icons.gps_fixed,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Positioned(
+            right: 20.0,
+            bottom: 125,
+            child: SizedBox(
+              height: 55,
+              width: 55,
+              child: FloatingActionButton(
+                backgroundColor: Color(0xFF01F982),
+                foregroundColor: Colors.black,
+                child: const Icon(
+                  Icons.push_pin,
+
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+
       ),
     );
   }
