bb593d8ee14e6420d5264c8f75c78f3d5d49f8f9
김동원 <dongwon0103@naver.com>
2022-06-18 15:43:39 +0900

    스토리 도메인 클래스, 스토리 리스트 프로바이더 추가

---
+++ lib/domain/story.dart
@@ -0,0 +1,24 @@
+class Story {
+  late String _title;
+  late String _user;
+  late String _date;
+  late String _time;
+  late String _youtubeLink;
+  late String _img;
+  late String _message;
+  late String _trackName;
+  late String _artist;
+
+  Story(this._title, this._user, this._date, this._time, this._youtubeLink,
+      this._img, this._message, this._trackName, this._artist);
+
+  String get title => _title;
+  String get user => _user;
+  String get date => _date;
+  String get time => _time;
+  String get link => _youtubeLink;
+  String get img => _img;
+  String get msg => _message;
+  String get track => _trackName;
+  String get artist => _artist;
+}
