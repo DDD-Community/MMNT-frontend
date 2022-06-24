fe974f8242976951468b7f8e0bebe8bad031e374
김동원 <dongwon0103@naver.com>
2022-06-24 15:37:41 +0900

    트랙과 본문 추가

---
+++ lib/showstory/component/storytext_widget.dart
@@ -0,0 +1,32 @@
+import 'package:dash_mement/style/story_textstyle.dart';
+import 'package:flutter/material.dart';
+
+class StoryText extends StatelessWidget {
+  late String _title;
+  late String _message;
+
+  StoryText(this._title, this._message) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        decoration: BoxDecoration(color: Color(0x99000000)),
+        child: Padding(
+            padding: EdgeInsets.all(22),
+            child: Column(children: [
+              Padding(
+                  padding: EdgeInsets.only(bottom: 28),
+                  child: Text(
+                    _title,
+                    style: StoryTextStyle().title,
+                    textAlign: TextAlign.center,
+                  )),
+              Text(
+                _message,
+                style: StoryTextStyle().message,
+                textAlign: TextAlign.center,
+              )
+            ])));
+  }
+}
