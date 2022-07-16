c73f9eb0f2d4a314643ebe2625631bdc0aab6087
praconfi <praconfi@gmail.com>
2022-07-16 16:10:00 +0900

    권한설정 관련 페이지 추가

--- lib/constants/style_constants.dart
+++ lib/constants/style_constants.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-TextStyle fabTextStyle = TextStyle(fontWeight: FontWeight.w500, fontSize: 11.sp);
+TextStyle kFabTextStyle = TextStyle(fontWeight: FontWeight.w500, fontSize: 11.sp);
+TextStyle kWhiteBold21 =  TextStyle(fontWeight: FontWeight.w600, fontSize: 21.sp);
+TextStyle kWhiteBold24 =  TextStyle(fontWeight: FontWeight.w600, fontSize: 24.sp);
+TextStyle kGray13 =  TextStyle(fontWeight: FontWeight.w300, fontSize: 13.sp, color: const Color(0xFF707077));
+TextStyle kGray14 =  TextStyle(fontWeight: FontWeight.w300, fontSize: 14.sp, color: const Color(0xFF707077));
+TextStyle kGray15 =  TextStyle(fontWeight: FontWeight.w400, fontSize: 15.sp, color: const Color(0xFF707077));
+TextStyle kGrayBold16 =  TextStyle(fontWeight: FontWeight.w600, fontSize: 16.sp, color: const Color(0xFF707077));
+Color kElevatedButtonColor = const Color(0xFF1E5EFF);
+Color kTextButtonColor = const Color(0xFF707077);
