258c4157c2c348cb4bb66e633ff6336afd70b2bb
김동원 <dongwon0103@naver.com>
2022-08-15 13:17:04 +0900

    git story modify

--- lib/showstory/show_story_arguments.dart
+++ lib/showstory/show_story_arguments.dart
@@ -2,5 +2,12 @@ class ShowStoryArguments {
   final String firstUrl;
   final double lat_y;
   final double lng_x;
-  ShowStoryArguments(this.firstUrl, this.lat_y, this.lng_x) {}
+  final int pin_idx;
+  final int length;
+  ShowStoryArguments(
+      {required this.firstUrl,
+      required this.lat_y,
+      required this.lng_x,
+      required this.pin_idx,
+      required this.length}) {}
 }
