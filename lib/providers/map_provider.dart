99869ced4d30191943d5f6e9652e9492525216e4
praconfi <praconfi@gmail.com>
2022-08-16 16:04:10 +0900

    현재 pin index, main moment 상태값 추가

--- lib/providers/map_provider.dart
+++ lib/providers/map_provider.dart
@@ -1,9 +1,19 @@
+import 'package:dash_mement/models/moment_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../models/pin_model.dart';
 
 class MapProvider extends ChangeNotifier {
   LatLng? currentLatLng;
+  int currentPinIndex = 0;
   String currentAddress = '지구 어딘가';
+  MomentModel mainMoment = MomentModel(music: '', artist: '', youtube_url: '', distance: 0, title: '', pin_idx: '', moment_idx: '');
+  List<PinModel> pinsList = [];
+
+  void updateMainMoment(MomentModel moment) {
+    mainMoment = moment;
+    notifyListeners();
+  }
 
   void updateCurrentAddress(String address) {
     currentAddress = address;
