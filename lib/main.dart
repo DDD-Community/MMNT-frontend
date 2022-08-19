bd900ea5872e5a26257e01e7fea075f4f322ae9f
praconfi <praconfi@gmail.com>
2022-08-19 20:01:32 +0900

    불필요한 코드 주석처리

--- lib/main.dart
+++ lib/main.dart
@@ -39,42 +39,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Story> _makeStoryTest() {
-    Story _s1 = Story(
-        "아주아주 힘든 어느 날",
-        "익명1",
-        DateTime.parse("2022-04-02 20:18:04Z"),
-        "https://www.youtube.com/watch?v=U3BVFY9wnTw",
-        "./assets/test/v1_test.jpg",
-        "밝게 빛나는 별이 되어 너를 비춰줄게 매일 어두워 헤맬 때도 앞이 두려워도\b 언제나 널 지켜줄게 세상이 널 아프게 할 땐 내가 안아줄게\n 매일포근한 바람으로 따듯한 햇살로난 널 지켜줄게 매일너의 모든 시간 속에 내가 함께할게",
-        "SUN GOES DOWN",
-        "Lil Nas X");
-    Story _s2 = Story(
-        "기분이 아주 좋아용",
-        "익명2",
-        DateTime.parse("2022-04-03 12:18:04Z"),
-        "https://www.youtube.com/watch?v=yQBImEeXNZ4",
-        "./assets/test/v2_test.jpg",
-        "내 하드 드라이브엔 히트곡이 가득하지 이런 나랑 같은 날 곡을 내려고? 다시 생각해보셔 너에겐 ‘지방시’가 필요한 게 아니라 예수님이 필요하지",
-        "First Class",
-        "Jack Harlow");
-    Story _s3 = Story(
-        "여친이랑 원소주 픽업하러 왔어용!",
-        "익명3",
-        DateTime.parse("2022-04-21 08:20:04Z"),
-        "https://www.youtube.com/watch?v=AwCvpQSUMBg",
-        "./assets/test/h1_test.jpg",
-        "오늘 여친이랑 더현대가서 원소주 사러 갔당가보니 제이팍도 있었다.이런 날은 글 남겨야징~",
-        "GANADARA (Feat. IU)",
-        "Jay Park");
-    List<Story> _storyList = [_s1, _s2, _s3];
-
-    return _storyList;
-  }
+  // List<Story> _makeStoryTest() {
+  //   Story _s1 = Story(
+  //       "아주아주 힘든 어느 날",
+  //       "익명1",
+  //       DateTime.parse("2022-04-02 20:18:04Z"),
+  //       "https://www.youtube.com/watch?v=U3BVFY9wnTw",
+  //       "./assets/test/v1_test.jpg",
+  //       "밝게 빛나는 별이 되어 너를 비춰줄게 매일 어두워 헤맬 때도 앞이 두려워도\b 언제나 널 지켜줄게 세상이 널 아프게 할 땐 내가 안아줄게\n 매일포근한 바람으로 따듯한 햇살로난 널 지켜줄게 매일너의 모든 시간 속에 내가 함께할게",
+  //       "SUN GOES DOWN",
+  //       "Lil Nas X");
+  //   Story _s2 = Story(
+  //       "기분이 아주 좋아용",
+  //       "익명2",
+  //       DateTime.parse("2022-04-03 12:18:04Z"),
+  //       "https://www.youtube.com/watch?v=yQBImEeXNZ4",
+  //       "./assets/test/v2_test.jpg",
+  //       "내 하드 드라이브엔 히트곡이 가득하지 이런 나랑 같은 날 곡을 내려고? 다시 생각해보셔 너에겐 ‘지방시’가 필요한 게 아니라 예수님이 필요하지",
+  //       "First Class",
+  //       "Jack Harlow");
+  //   Story _s3 = Story(
+  //       "여친이랑 원소주 픽업하러 왔어용!",
+  //       "익명3",
+  //       DateTime.parse("2022-04-21 08:20:04Z"),
+  //       "https://www.youtube.com/watch?v=AwCvpQSUMBg",
+  //       "./assets/test/h1_test.jpg",
+  //       "오늘 여친이랑 더현대가서 원소주 사러 갔당가보니 제이팍도 있었다.이런 날은 글 남겨야징~",
+  //       "GANADARA (Feat. IU)",
+  //       "Jay Park");
+  //   List<Story> _storyList = [_s1, _s2, _s3];
+  //
+  //   return _storyList;
+  // }
 
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    List<Story> _storyList = _makeStoryTest();
+    // List<Story> _storyList = _makeStoryTest();
 
     return MultiProvider(
       providers: [
