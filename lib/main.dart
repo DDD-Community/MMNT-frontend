dd959e3c85c7589f34ab1fa73dc6c475c9f80972
praconfi <praconfi@gmail.com>
2022-08-24 13:35:02 +0900

    hot reload시에서 초기화면으로 돌아오는문제로 디버그모드에서는 스플래스 FutureBuilder제외

--- lib/main.dart
+++ lib/main.dart
@@ -38,89 +38,100 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return FutureBuilder(
-        future: Init.instance.initialize(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: Splash(),
-            );
-          } else {
-            return ScreenUtilInit(
-              designSize: const Size(375, 812),
-              builder: (BuildContext context, Widget? child) {
-                return
-                  MultiProvider(
-                    providers: [
-                      ChangeNotifierProvider(create: (_) => AppProvider()),
-                      ChangeNotifierProvider(create: (_) => MapProvider()),
-                      ChangeNotifierProvider(create: (_) => SlidingPanelProvider()),
-                      ChangeNotifierProvider(create: (_) => StoryListProvider()),
-                      ChangeNotifierProvider(create: (_) => PushStoryProvider())
-                    ],
-                    child: MaterialApp(
-                    title: 'dash_moment',
-                    debugShowCheckedModeBanner: false,
 
-                    // Dark theme 기반
-                    theme: ThemeData(
-                        fontFamily: 'Pretendard',
-                        brightness: Brightness.dark,
-                        highlightColor: Colors.yellow,
-                        scaffoldBackgroundColor: Colors.black,
-                        appBarTheme: AppBarTheme(
-                            centerTitle: true,
-                            titleTextStyle: kGrayBold18,
-                            color: Colors.black,
-                            iconTheme: IconThemeData(
-                              color: kAppbarIconColor,
-                            )),
-                        floatingActionButtonTheme:
-                            const FloatingActionButtonThemeData(
-                          backgroundColor: Color(0xFF1E5EFF),
-                        ),
-                        textTheme: TextTheme(
-                          // bodyText2가 기본 텍스트 스타일
-                          bodyText2: TextStyle(
-                              fontSize: 16.sp, fontWeight: FontWeight.w700),
-                        ),
-                        elevatedButtonTheme: ElevatedButtonThemeData(
-                            style: ElevatedButton.styleFrom(
-                                primary: kElevatedButtonColor)),
-                        textButtonTheme: TextButtonThemeData(
-                            style: TextButton.styleFrom(
-                                primary: const Color(0xFF707077),
-                                textStyle: TextStyle(fontSize: 15.sp))),
-                        inputDecorationTheme: InputDecorationTheme(
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: kTextFormFieldUnderlineColor,
-                                    width: 1.5)))),
-                    initialRoute: MapScreen.routeName,
-                    routes: {
-                      LoginScreen.routeName: (context) => const LoginScreen(),
-                      SignUpScreen.routeName: (context) => const SignUpScreen(),
-                      SignInScreen.routeName: (context) => const SignInScreen(),
-                      MapScreen.routeName: (context) => MapScreen(),
-                      ShowStory.routeName: (context) => ShowStory(
-                          ModalRoute.of(context)!.settings.arguments
-                              as ShowStoryArguments),
-                      UserPage.routeName: (context) => UserPage(),
-                      ShowStoryTest.routeName: (context) => ShowStoryTest(),
-                      UserHistory.routeName: (context) => UserHistory(),
-                      PinCreateScreen.routeName: (context) => PinCreateScreen(),
-                    },
+    if (kDebugMode) {
+      return const MmntApp();
+    }
+    return FutureBuilder(
+      future: Init.instance.initialize(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Splash(),
+          );
+        } else {
+          return const MmntApp();
+        }
+      },
+    );
+  }
+}
+
+class MmntApp extends StatelessWidget {
+  const MmntApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => AppProvider()),
+            ChangeNotifierProvider(create: (_) => MapProvider()),
+            ChangeNotifierProvider(create: (_) => SlidingPanelProvider()),
+            ChangeNotifierProvider(create: (_) => StoryListProvider()),
+            ChangeNotifierProvider(create: (_) => PushStoryProvider())
+          ],
+          child: MaterialApp(
+            title: 'dash_moment',
+            debugShowCheckedModeBanner: false,
+
+            // Dark theme 기반
+            theme: ThemeData(
+                fontFamily: 'Pretendard',
+                brightness: Brightness.dark,
+                highlightColor: Colors.yellow,
+                scaffoldBackgroundColor: Colors.black,
+                appBarTheme: AppBarTheme(
+                    centerTitle: true,
+                    titleTextStyle: kGrayBold18,
+                    color: Colors.black,
+                    iconTheme: IconThemeData(
+                      color: kAppbarIconColor,
+                    )),
+                floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                  backgroundColor: Color(0xFF1E5EFF),
                 ),
-                  );
-              },
-            );
-          }
-        },
+                textTheme: TextTheme(
+                  // bodyText2가 기본 텍스트 스타일
+                  bodyText2:
+                      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w700),
+                ),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                        primary: kElevatedButtonColor)),
+                textButtonTheme: TextButtonThemeData(
+                    style: TextButton.styleFrom(
+                        primary: const Color(0xFF707077),
+                        textStyle: TextStyle(fontSize: 15.sp))),
+                inputDecorationTheme: InputDecorationTheme(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: kTextFormFieldUnderlineColor, width: 1.5)))),
+            initialRoute: LoginScreen.routeName,
+            routes: {
+              LoginScreen.routeName: (context) => const LoginScreen(),
+              SignUpScreen.routeName: (context) => const SignUpScreen(),
+              SignInScreen.routeName: (context) => const SignInScreen(),
+              MapScreen.routeName: (context) => MapScreen(),
+              ShowStory.routeName: (context) => ShowStory(
+                  ModalRoute.of(context)!.settings.arguments
+                      as ShowStoryArguments),
+              UserPage.routeName: (context) => UserPage(),
+              ShowStoryTest.routeName: (context) => ShowStoryTest(),
+              UserHistory.routeName: (context) => UserHistory(),
+              PinCreateScreen.routeName: (context) => PinCreateScreen(),
+            },
+          ),
+        );
+      },
     );
   }
 }
@@ -132,6 +143,6 @@ class Init {
   Future initialize() async {
     // This is where you can initialize the resources needed by your app while
     await Future.delayed(
-        kDebugMode ? const Duration(seconds: 0) : const Duration(seconds: 4));
+        kDebugMode ? const Duration(seconds: 4) : const Duration(seconds: 4));
   }
 }
