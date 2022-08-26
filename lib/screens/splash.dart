28eb212267ccac0322ff4d2f853ef1b2eeb006c5
praconfi <praconfi@gmail.com>
2022-08-26 13:16:33 +0900

    스플래시 변경

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Splash extends StatelessWidget {
   static const id = '/splash';
@@ -7,6 +8,6 @@ class Splash extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1E5EFF),
-      body: Center(child: Image.asset('assets/gif/splash.gif')));
+      body: Center(child: Image.asset('assets/gif/splash_logo.gif')));
   }
 }
