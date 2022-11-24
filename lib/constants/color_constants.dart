5132d5d10d3c8df52fd11a23d01f7cd772452c80
praconfi <praconfi@gmail.com>
2022-11-24 18:28:02 +0900

    컬러 디자인 시스템 적용준비

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Color design system
+const Color kPrimaryColor = Color(0xFF1E5EFF);
+const Color kCardBgColor = Color(0xFF1E1E21);
+const Color kButtonBgColor = Color(0xFF2E2F35);
+const Color kDisabledColor = Color(0xFF707077);
+const Color CardBgColor2 = Color(0xFFF9E9FA9);
+const Color kDisabledColor2 = Color(0xFF9E9FA9);
+const Color kBlue300 = Color(0xFF5894FC);
+const Color kBlue200 = Color(0xFF627DAB);
+const Color kBlue100 = Color(0xFF627DAB);
+const Color kErrorColor = Color(0xFFFF6060);
+
+
+
+
+// const Color kGray = Color(0xFF707077);
+// const Color kTextButtonColor = kGray;
+const Color kTextFormFieldUnderlineColor = Color(0xff1E5EFF);
+const Color kAppbarIconColor = Color(0xffbD9D9D9);
